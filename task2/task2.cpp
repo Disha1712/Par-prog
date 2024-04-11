aa31bdab7b66bcbebf670795e887bd6cb1d1d4bf
Disha Taneja <taneja.disha89@gmail.com>
2024-04-11 20:04:52 +0530

    code updated

--- task2/task2.cpp
+++ task2/task2.cpp
@@ -9,12 +9,13 @@
 using namespace std;
 #define MOD 1000000007
 
-vector<int64_t> bellman_ford(long long int Vertices, vector<vector<int>>& edges, int Start) {
-    vector<int64_t> dist(Vertices,INT_MAX);
-    vector<int64_t> negative_cycle;
-    negative_cycle.push_back(-1);
+int64_t* bellman_ford(long long int Vertices,int** edges, int Start,int num_edges) {
+    int64_t* dist=new int64_t[Vertices];
+    int64_t* negative_cycle=new int64_t[1];
+    negative_cycle[0]=-1;
+    for (int i=0;i<Vertices;++i)
+        dist[i]=INT_MAX;
     dist[Start]=0;
-    int num_edges=edges.size();
     for (int i=1;i<=Vertices;i++){
         #pragma omp parallel for 
         for (int j=0;j<num_edges;j++){
@@ -25,33 +26,44 @@ vector<int64_t> bellman_ford(long long int Vertices, vector<vector<int>>& edges,
                 dist[destination]=dist[source]+weight;
             }
         }
+        if (i%1000==0)  cout<<i<<endl;
     }
     for (int i=0;i<num_edges;i++){
         int source=edges[i][0];
         int destination=edges[i][1];
         int weight=edges[i][2];
         if ((dist[source]+weight)<dist[destination]){
+            delete[] dist;
             return negative_cycle;
         }
     }
     return dist;
 }
 
-vector<vector<int>> createGraph(const string& filename) {
+pair<int**,int> createGraph(const string& filename) {
     ifstream inputFile(filename);
     vector<vector<int>> graph;
     string line;
-    while (getline(inputFile, line)) {
-        int source, destination, weight;
+    int num_edges=0;
+    while (getline(inputFile,line)) {
+        int source,destination,weight;
         istringstream iss(line);
         if (!(iss >> source >> destination >> weight)) { 
             cerr << "Error reading line: " << line << endl;
             continue;
         }
-        graph.push_back({source, destination, weight});
+        graph.push_back({source,destination,weight});
+        num_edges++;
     }
     inputFile.close();
-    return graph;
+    int** edges=new int*[num_edges];
+    for (int i=0;i<num_edges;i++) {
+        edges[i]=new int[3];
+        for (int j=0;j<3;j++) {
+            edges[i][j]=graph[i][j];
+        }
+    }
+    return make_pair(edges,num_edges);
 }
 
 int main(int argc, char *argv[]) {
@@ -60,21 +72,27 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     string filename = argv[1];
-    vector<vector<int>> graph = createGraph(filename);
-    set <int> vert;
-    for (const auto& edge : graph) {
-        vert.insert(edge[0]);
-        vert.insert(edge[1]);
+    pair<int**,int> graphAndEdge=createGraph(filename);
+    int** graph=graphAndEdge.first;
+    int num_edges=graphAndEdge.second;
+    set <int>vert;
+    for (int i=0;i<num_edges;i++){
+        vert.insert(graph[i][0]);
+        vert.insert(graph[i][1]);
     }
     long long int v=vert.size();
     for (int num_threads=2;num_threads<=2048;num_threads*=2) {
         omp_set_num_threads(num_threads);
-        auto time_start= chrono::high_resolution_clock::now();  
-        vector<int64_t> dist=bellman_ford(v,graph,0);
+        auto time_start= chrono::high_resolution_clock::now(); 
+        int64_t* dist=bellman_ford(v,graph,0,num_edges);
         auto time_end=chrono::high_resolution_clock::now();
         chrono::duration<double> time=time_end-time_start;
         cout<<num_threads<<"\t\t"<<time.count()<<endl;
+        delete [] dist;
 
     }
+    for (int i = 0; i < num_edges; ++i)
+        delete[] graph[i];
+    delete[] graph;
     return 0;
 }
