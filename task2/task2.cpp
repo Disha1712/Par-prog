9f71793d2788ad86c6f4364b94c0dcfcbdba6539
Disha Taneja <taneja.disha89@gmail.com>
2024-04-18 18:11:43 +0530

    code updated

--- task2/task2.cpp
+++ task2/task2.cpp
@@ -6,32 +6,38 @@
 #include <set>
 #include <climits>
 #include <chrono>
+typedef long long int ll;
 using namespace std;
-typedef long long ll;
+#define mod 1000000007
 
-int64_t* bellman_ford(int Vertices,int** edges, int Start,ll num_edges) {
-    int64_t* dist=new int64_t[Vertices];
-    int64_t* negative_cycle=new int64_t[1];
+ll* bellman_ford(ll Vertices,ll** edges, int Start,ll num_edges) {
+    ll* dist=new ll[Vertices];
+    ll* negative_cycle=new ll[1];
     negative_cycle[0]=-1;
     for (int i=0;i<Vertices;++i)
         dist[i]=LLONG_MAX;
     dist[Start]=0;
-    #pragma omp parallel for collapse(2)
-    for (int i=1;i<=Vertices;i++){
+    int ival=0;
+    #pragma omp parallel for reduction(+:ival)
+    for (ll i=1;i<=Vertices;i++){
+        bool flag=false;
         for (ll j=0;j<num_edges;j++){
-            int source=edges[j][0];
-            int destination=edges[j][1];
-            int weight=edges[j][2];
-            if (dist[source]!=LLONG_MAX && (dist[source]+weight)<dist[destination]){
-                dist[destination]=dist[source]+weight;
+            ll source=edges[j][0];
+            ll destination=edges[j][1];
+            ll weight=edges[j][2];
+            if (dist[source]!=LLONG_MAX && ((dist[source]+(weight%mod))%mod)<dist[destination]){
+                dist[destination]=(dist[source]+(weight%mod))%mod;
+		flag=true;
             }
         }
+        if (flag)	ival++;
     }
+    cout<<"Values of i for which there is a change in distance array: "<<ival<<endl;
     for (ll i=0;i<num_edges;i++){
-        int source=edges[i][0];
-        int destination=edges[i][1];
-        int weight=edges[i][2];
-        if (dist[source]!=LLONG_MAX && (dist[source]+weight)<dist[destination]){
+        ll source=edges[i][0];
+        ll destination=edges[i][1];
+        ll weight=edges[i][2];
+        if (dist[source]!=LLONG_MAX && ((dist[source]+(weight%mod))%mod)<dist[destination]){
             delete[] dist;
             return negative_cycle;
         }
@@ -39,13 +45,13 @@ int64_t* bellman_ford(int Vertices,int** edges, int Start,ll num_edges) {
     return dist;
 }
 
-pair<int**,ll> createGraph(const string& filename) {
+pair<ll**,ll> createGraph(const string& filename) {
     ifstream inputFile(filename);
-    vector<vector<int>> graph;
+    vector<vector<ll>> graph;
     string line;
     ll num_edges=0;
     while (getline(inputFile,line)) {
-        int source,destination,weight;
+        ll source,destination,weight;
         istringstream iss(line);
         if (!(iss >> source >> destination >> weight)) { 
             cerr << "Error reading line: " << line << endl;
@@ -55,9 +61,9 @@ pair<int**,ll> createGraph(const string& filename) {
         num_edges++;
     }
     inputFile.close();
-    int** edges=new int*[num_edges];
+    ll** edges=new ll*[num_edges];
     for (int i=0;i<num_edges;i++) {
-        edges[i]=new int[3];
+        edges[i]=new ll[3];
         for (int j=0;j<3;j++) {
             edges[i][j]=graph[i][j];
         }
@@ -71,19 +77,23 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     string filename = argv[1];
-    pair<int**,ll> graphAndEdge=createGraph(filename);
-    int** graph=graphAndEdge.first;
-    int num_edges=graphAndEdge.second;
+    pair<ll**,ll> graphAndEdge=createGraph(filename);
+    ll** graph=graphAndEdge.first;
+    ll num_edges=graphAndEdge.second;
     set <int>vert;
     for (ll i=0;i<num_edges;i++){
         vert.insert(graph[i][0]);
         vert.insert(graph[i][1]);
     }
-    int v=vert.size();
+    ll v=vert.size();
     for (int num_threads=2;num_threads<=2048;num_threads*=2) {
         omp_set_num_threads(num_threads);
         auto time_start= chrono::high_resolution_clock::now(); 
-        int64_t* dist=bellman_ford(v,graph,0,num_edges);
+        ll* dist=bellman_ford(v,graph,0,num_edges);
+	for (int i=0;i<v;i++){
+	   cout<<dist[i]<<"  "; 
+        }
+	cout<<endl;
         auto time_end=chrono::high_resolution_clock::now();
         chrono::duration<double> time=time_end-time_start;
         cout<<num_threads<<"\t\t"<<time.count()<<endl;
